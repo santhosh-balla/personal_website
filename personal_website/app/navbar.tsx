759c99ad66dba61283f8d217f02e835193c0a544
santhosh-balla <satyasanthosh31@gmail.com>
2026-01-27 14:07:07 -0500

    used flexbox to even out navbar, and changed colors

--- personal_website/app/navbar.tsx
+++ personal_website/app/navbar.tsx
@@ -12,68 +12,71 @@
 import Link from "next/link";
 
 function NavbarButtons({
-    link,
-    name,
-    className,
+  link,
+  name,
+ 
 }: {
-    link: string;
-    name: string;
-    className: string;
+  link: string;
+  name: string;
+  
 }) {
-    return (
-        <Link href={link} className={className}>
-            {name}
-        </Link>
-    );
+  return (
+    <Link style ={{ color: 'white' }}href={link}>
+      {name}
+    </Link>
+  );
 }
 
 function Socials({
-    link,
-    img,
-    size = 70,
-    alt = "exampleImg",
+
+  link,
+  img,
+  size = 70,
+  alt = "exampleImg",
 }: {
-    link: string;
-    img?: string;
-    size: number;
-    alt?: string;
+  link: string;
+  img?: string;
+  size: number;
+  alt?: string;
+
 }) {
-    return (
-        <>
-            <a href={link} target="_blank">
-                <img width={size} src={img} alt={alt}></img>
-            </a>
-        </>
-    );
+  return (
+    <div>
+      <a href={link} target="_blank">
+        <img width={size} src={img} alt={alt}></img>
+      </a>
+    </div>
+  );
 }
 
 export default function NavBar() {
-    return (
-        <>
-            <NavbarButtons link="/blog" name={"Blog"} className="navbar-component" />
-            <NavbarButtons
-                link="/projects"
-                name={"Projects"}
-                className="navbar-component"
-            />
-            <NavbarButtons
-                link="/aboutme"
-                name={"About Me"}
-                className="navbar-component"
-            />
+  return (
+    <div className = 'navbar-container'>
+      <NavbarButtons link="/blog" name={"Blog"} />
+      <NavbarButtons
+        
+        link="/projects"
+        name={"Projects"}
+      />
+      <NavbarButtons
+       
+        link="/aboutme"
+        name={"About Me"}
+      />
 
-            <Socials
-                link="https://www.linkedin.com/in/santhosh-balla5579/"
-                size={100}
-                alt="LinkedIn"
-                img="/in-logo/LI-Logo.png"
-            ></Socials>
-            <Socials
-                link="https://github.com/santhosh-balla"
-                size={100}
-                alt="GitHub"
-                img="GitHub_Logos/SVG/GitHub_Lockup_Black_Clearspace.svg"
-            ></Socials>
-        </>
-    );
+      <Socials
+       
+        link="https://www.linkedin.com/in/santhosh-balla5579/"
+        size={100}
+        alt="LinkedIn"
+        img="/in-logo/LI-Logo.png"
+      ></Socials>
+      <Socials
+        link="https://github.com/santhosh-balla"
+        size={100}
+        alt="GitHub"
+        img="GitHub_Logos/SVG/GitHub_Lockup_Black_Clearspace.svg"
+      ></Socials>
+    </div>
+  );
 }
